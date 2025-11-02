a564322ec60802c04348d38c6a61d4b9a264ec2f
tiwvinish <tiwvinish@gmail.com>
2025-11-02 20:14:48 +0545

    message added

--- api/server.js
+++ api/server.js
@@ -80,7 +80,7 @@ mongoose.connect(process.env.MONGO_URI)
     
 
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT} !!`);
     });
   })
   .catch((err) => {
